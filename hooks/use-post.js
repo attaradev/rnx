27efbab6224dc8bc5a64030f8f6f9c7e6b0d4f17
Mike Perry Y Attara <mpyebattara@gmail.com>
2020-09-21 01:26:10 +0000

    diplay post

---
+++ hooks/use-post.js
@@ -0,0 +1,7 @@
+import { useContext } from "react";
+import { PostsContext } from "../contexts/posts-context";
+
+export function usePost(id) {
+  const posts = useContext(PostsContext);
+  return posts.find((post) => post.id === id);
+}
--- pages/post/[id].js
+++ pages/post/[id].js
@@ -1,10 +1,17 @@
 import { useRouter } from "next/router";
+import { usePost } from "../../hooks/use-post";
 
 const Post = () => {
   const router = useRouter();
   const { id } = router.query;
+  const { title, body } = usePost(id);
 
-  return <p>Post: {id}</p>;
+  return (
+    <div>
+      <h2>{title}</h2>
+      <p>{body}</p>
+    </div>
+  );
 };
 
 export default Post;
